8d1c8d09bd7ad2634ca6145f502048d7845564e7
Silvia <silviakavabata@gmail.com>
2023-09-05 20:01:08 -0300

    Importando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import Nav from './Nav'
+import Home from './Home'
+import Produtos from './Produtos'
+import Sobre from './Sobre'
+import Footer from './Footer'
 
 function App() {
-
-
   return (
     <>
-      
+    <BrowserRouter>
+    <Nav />
+    <Routes>
+
+    </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   )
 }
