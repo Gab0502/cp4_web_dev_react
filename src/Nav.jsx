ced60f0b7b84015ff3492694b7151e26d820a555
Enricco Rossi <enriccorossi@gmail.com>
2023-09-06 19:25:56 -0300

    Criação do NavLink

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,6 +4,9 @@ import {NavLink} from 'react-router-dom'
 function Nav(){
     return(
         <>
+        <NavLink to="/">Home|</NavLink>
+        <NavLink to="/Produtos">Produtos|</NavLink>
+        <NavLink to="/Sobre">Sobre|</NavLink>
         </>
     )
 }
