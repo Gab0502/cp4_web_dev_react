7765ca6ec4fb0504a0b74610988aed8e9fe67825
guilherme <gnbernnardo@gmail.com>
2023-09-08 00:06:36 -0300

    Criando footer e ajuste no nav

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,7 @@ import {} from 'react'
 function Footer(){
     return(
         <>
+        <p>Telefone: +55 11 3385 8010 | E-mail: contato@fiap.com.br</p>
         </>
     )
 }
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,9 +4,9 @@ import {NavLink} from 'react-router-dom'
 function Nav(){
     return(
         <>
-        <NavLink to="/Home">Home|</NavLink>
-        <NavLink to="/Produtos">Produtos|</NavLink>
-        <NavLink to="/Sobre">Sobre|</NavLink>
+        <NavLink to="/Home">Home | </NavLink>
+        <NavLink to="/Produtos">Produtos | </NavLink>
+        <NavLink to="/Sobre">Sobre</NavLink>
         </>
     )
 }
