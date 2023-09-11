99063fd2623edc00b27c5878b89689857ce48577
CwalterS/ <dev_caiquews@outlook.com>
2023-09-11 18:50:10 -0300

    switchDescs

--- src/Produtos.jsx
+++ src/Produtos.jsx
@@ -9,17 +9,19 @@ function Produtos(){
     return(
         <>
         <div id="container-produtos">
-        <h3>VINHO TINTO</h3>
+        
         <img src = {imgtinto} alt="tinto" width = "1000px" heigth = "1000px"/>
+        <label>Produto: Vinho Tinto</label>
         
-        <h3>VINHO BRANCO</h3>
+        
         <img src = {imgbranco} alt="branco" width = "1000px" heigth = "1000px"/>
+        <label>Produto: Vinho Branco</label>
         
-        <h3>VINHO ESPUMANTE</h3>
         <img src = {imgespumante} alt="espumante" width = "1000px" heigth = "1000px"/>
-        
-        <h3>VINHO ROSE</h3>
+        <label>Produto: Vinho Espumante</label>
+
         <img src = {imgrose} alt="rose" width = "1000px" heigth = "1000px"/>
+        <label>Produto: Vinho Rose</label>
         </div>
         </>
     )
